8dd89d1c7c04cd03a223cbc3277650727c69dd57
Duhan <107396918+duhanbektas@users.noreply.github.com>
2022-08-17 15:36:37 +0300

    Add files via upload

--- src/components/Die.js
+++ src/components/Die.js
@@ -1,19 +1,16 @@
-import React from "react";
+import React from "react"
 
 export default function Die(props) {
-
-  return (
-    // if it's held (isHeld === true), its background color
-    // changes to a light green
-    <div
-      className={props.isHeld ? "die-face isHeld" : "die-face"}
-      onClick={props.holdDice}
-      style={{
-        backgroundImage: `url(${dieFace})`,
-        backgroundSize: "cover",
-      }}
-    >
-      {/* <h2 className="die-num">{props.value}</h2> */}
-    </div>
-  );
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391" : "white"
+    }
+    return (
+        <div 
+            className="die-face" 
+            style={styles}
+            onClick={props.holdDice}
+        >
+            <h2 className="die-num">{props.value}</h2>
+        </div>
+    )
 }
